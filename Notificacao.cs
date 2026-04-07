169f17ff67e00688c63924c9a4fff604a638535d
tsmelo <ts.melo@unifesp.br>
2026-04-07 17:00:04 -0300

    commit

---
+++ Notificacao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public abstract class Notificacao
+    {
+        public abstract void Enviar(string mensagem, string destino);
+    }
+}
