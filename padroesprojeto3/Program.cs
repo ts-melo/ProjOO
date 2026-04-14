548e8340027d0ab8f1331e3618ae1c82a0dd7614
tsmelo <ts.melo@unifesp.br>
2026-04-14 16:31:59 -0300

    add tv

---
+++ padroesprojeto3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExHomeTheater
+{
+    //subsistemas
+
+    public class televisao
+    {
+        public void Ligar()=> Console.WriteLine("Tv ligada");
+        public void Desligar()=> Console.WriteLine("Tv desligada");
+        public void ConfigHDMI()=> Console.WriteLine("HDMI configurado");
+    }
+
+    
+}
