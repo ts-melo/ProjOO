137a1e8f23ca9817b0ad23691849280737907222
tsmelo <ts.melo@unifesp.br>
2026-04-14 16:40:29 -0300

    resto dos sistemas

--- padroesprojeto3/Program.cs
+++ padroesprojeto3/Program.cs
@@ -2,7 +2,7 @@
 
 namespace ExHomeTheater
 {
-    //subsistemas
+    //subsistemas ou dispositivos
 
     public class televisao
     {
@@ -11,5 +11,27 @@ namespace ExHomeTheater
         public void ConfigHDMI()=> Console.WriteLine("HDMI configurado");
     }
 
-    
+    public class som
+    {
+        public void Ligar()=> Console.Write("Som ligado");
+        public void Desligar()=> Console.Write("Som desligado");
+        public void ConfigurarVolume(int volume)=> Console.WriteLine($"Volume configurado para {volume}");        
+    }
+
+    public class playerMedia
+    {
+        public void Ligar()=> Console.Write("Ligando sistema de midia");
+        public void Desligar()=> Console.Write("Desligando sistema de midia");
+        public void Play(string filme)=> Console.WriteLine($"Reproduzindo filme: {filme}");
+        
+    }
+
+    public class luzAmbiente
+    {
+        public void Ligar()=> Console.WriteLine("Luz ambiente ligada");
+        public void Desligar()=> Console.WriteLine("Luz ambiente desligada");
+        public void ConfigurarIntensidade(int intensidade)=> Console.WriteLine($"Intensidade da luz configurada para {intensidade}");
+        
+    }
+
 }
